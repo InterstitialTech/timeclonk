5aa8e3d33cbe260fd34ce1d6aaf90cea2347c231
Ben Burdette <bburdette@gmail.com>
2021-11-20 13:39:18 -0700

    fix

--- elm/elm-srcs.nix
+++ elm/elm-srcs.nix
@@ -1,24 +1,117 @@
-elm2nix 0.2.1
+{
 
-Usage: elm2nix COMMAND
-  Convert Elm project to Nix expressions
+      "mdgriffith/elm-ui" = {
+        sha256 = "0ffcqv4a4ad400hwp824m3qq4jy82cqp5ghmhp1m0q7n004z6kgv";
+        version = "1.1.8";
+      };
 
-Available options:
-  -h,--help                Show this help text
+      "elm/file" = {
+        sha256 = "1rljcb41dl97myidyjih2yliyzddkr2m7n74x7gg46rcw4jl0ny8";
+        version = "1.0.5";
+      };
 
-Available commands:
-  init                     Generate default.nix (printed to stdout)
-  convert                  Generate Nix expressions for elm.json using
-                           nix-prefetch-url
-  snapshot                 Generate versions.dat
+      "bburdette/toop" = {
+        sha256 = "091pzp712fwdwqkrikd4yprs5kdfix5q297jz0y9pkpy6pgqfwqw";
+        version = "1.0.1";
+      };
 
+      "elm/json" = {
+        sha256 = "0kjwrz195z84kwywaxhhlnpl3p251qlbm5iz6byd6jky2crmyqyh";
+        version = "1.1.3";
+      };
 
+      "elm/html" = {
+        sha256 = "1n3gpzmpqqdsldys4ipgyl1zacn0kbpc3g4v3hdpiyfjlgh8bf3k";
+        version = "1.0.0";
+      };
 
-    Usage:
+      "bburdette/windowkeys" = {
+        sha256 = "02bjr39aqvqkpdbmsyiarhn9bc3jqgnqbcfhb1apdf8fmpj1b5hv";
+        version = "1.0.1";
+      };
 
-      $ elm2nix init > default.nix
-      $ elm2nix convert > elm-srcs.nix
-      $ nix-build
+      "elm/parser" = {
+        sha256 = "0a3cxrvbm7mwg9ykynhp7vjid58zsw03r63qxipxp3z09qks7512";
+        version = "1.1.0";
+      };
 
-      Note: You have to run elm2nix from top-level directory of an Elm project.
-    
+      "elm/browser" = {
+        sha256 = "0nagb9ajacxbbg985r4k9h0jadqpp0gp84nm94kcgbr5sf8i9x13";
+        version = "1.0.2";
+      };
+
+      "TSFoster/elm-uuid" = {
+        sha256 = "1a4a7hlp7mynbng9sykicm48zjwkw9w8bsik0zskgwa06qsmjq85";
+        version = "4.1.0";
+      };
+
+      "myrho/elm-round" = {
+        sha256 = "1ghw75fq2p316gw95b3mknfhkq7196fc6ziy6c5ic34gxfb03zvg";
+        version = "1.0.4";
+      };
+
+      "elm/core" = {
+        sha256 = "19w0iisdd66ywjayyga4kv2p1v9rxzqjaxhckp8ni6n8i0fb2dvf";
+        version = "1.0.5";
+      };
+
+      "PanagiotisGeorgiadis/elm-datetime" = {
+        sha256 = "10fd2pad1kv8gm8zj2ixspyyxjqlzx50s3di17vl51gp41j05qbw";
+        version = "1.3.0";
+      };
+
+      "elm/url" = {
+        sha256 = "0av8x5syid40sgpl5vd7pry2rq0q4pga28b4yykn9gd9v12rs3l4";
+        version = "1.0.0";
+      };
+
+      "elm/random" = {
+        sha256 = "138n2455wdjwa657w6sjq18wx2r0k60ibpc4frhbqr50sncxrfdl";
+        version = "1.0.0";
+      };
+
+      "elm/http" = {
+        sha256 = "008bs76mnp48b4dw8qwjj4fyvzbxvlrl4xpa2qh1gg2kfwyw56v1";
+        version = "2.0.0";
+      };
+
+      "elm/time" = {
+        sha256 = "0vch7i86vn0x8b850w1p69vplll1bnbkp8s383z7pinyg94cm2z1";
+        version = "1.0.0";
+      };
+
+      "elm/bytes" = {
+        sha256 = "02ywbf52akvxclpxwj9n04jydajcbsbcbsnjs53yjc5lwck3abwj";
+        version = "1.0.8";
+      };
+
+      "TSFoster/elm-bytes-extra" = {
+        sha256 = "1ip5br0kszhr8kmk19jsqqqhgna89drn3cwb6zn8ncwfrxc8xvs9";
+        version = "1.3.0";
+      };
+
+      "rtfeldman/elm-hex" = {
+        sha256 = "1y0aa16asvwdqmgbskh5iba6psp43lkcjjw9mgzj3gsrg33lp00d";
+        version = "1.0.0";
+      };
+
+      "TSFoster/elm-md5" = {
+        sha256 = "0jgy74sbqqj7gk7a6hh3cv1pnglnpyqbyg8wyf9s9bsipb2zcrrp";
+        version = "2.0.1";
+      };
+
+      "TSFoster/elm-sha1" = {
+        sha256 = "1m2r350gd1xxc7dxzn8dwjv3ir398kigrsspv6agqaasls9n12v6";
+        version = "2.1.1";
+      };
+
+      "danfishgold/base64-bytes" = {
+        sha256 = "1xw4kq7r4zpx484wcnwwkcgzmr4wdxsv3i0j90ynnrjc68ydwgj1";
+        version = "1.1.0";
+      };
+
+      "elm/virtual-dom" = {
+        sha256 = "0q1v5gi4g336bzz1lgwpn5b1639lrn63d8y6k6pimcyismp2i1yg";
+        version = "1.0.2";
+      };
+}
