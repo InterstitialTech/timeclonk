efa6ca18fdb766dbad11607c6bc3793c3e792d74
Ben Burdette <bburdette@gmail.com>
2022-06-03 07:28:14 -0600

    remove obsolete config

--- server/src/config.rs
+++ server/src/config.rs
@@ -6,9 +6,6 @@ use std::path::PathBuf;
 pub struct Config {
   pub ip: String,
   pub port: u16,
-  // pub createdirs: bool,
-  // pub altmainsite: Vec<String>,
   pub static_path: Option<PathBuf>,
-  // pub error_index_note: Option<i64>,
   pub orgauth_config: orgauth_data::Config,
 }
